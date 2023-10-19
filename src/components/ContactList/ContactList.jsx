e143fc237937b8a99c6097854f19c2f30f466ab8
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-10-20 01:06:56 +0200

    .

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class ContactList extends Component{
+
+    render() {
+        return (
+            <ul>
+                
+            </ul>
+        )
+    }
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+export class Filter extends Component{
+
+    render() {
+        return (
+            <input/>
+        )
+    }
+}
